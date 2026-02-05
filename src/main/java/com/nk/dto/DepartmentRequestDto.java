387b9c0672ca137f0f18c4bd3f0937f5aadeb40c
Venkata Kishor <venkatakishor1677@gmail.com>
2026-02-05 12:17:20 +0530

    provided annotations for validations

--- src/main/java/com/nk/dto/DepartmentRequestDto.java
+++ src/main/java/com/nk/dto/DepartmentRequestDto.java
@@ -1,10 +1,21 @@
 package com.nk.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class DepartmentRequestDto {
+
+    @NotNull
+    @NotBlank(message = " department name should not be blank")
     private String deptName;
+
+    @NotNull
+    @NotBlank(message = "Location should not be blank")
     private String location;
+
+    @NotNull
+    @NotBlank(message = "Manager should not be blank")
     private String managerName;
 }
